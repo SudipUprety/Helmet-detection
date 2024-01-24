4f812704c994d6f550b185e878fe8b92846c4c82
1su-pixek <sudipuprety1@gmail.com>
2024-01-24 20:24:05 +0545

    change in code

--- hbn.py
+++ hbn.py
@@ -23,7 +23,7 @@ with open("helmet.names", "r") as f:
 layer_names_helmet = net_helmet.getLayerNames()
 output_layers_helmet = [layer_names_helmet[i - 1] for i in net_helmet.getUnconnectedOutLayers()]
 
-cap = cv2.VideoCapture('video.mp4')
+cap = cv2.VideoCapture('video.MOV')
 
 # Create a directory to save license plate images
 if not os.path.exists('license_plate_images'):
