8bf630a609027cc15132168dad11946fbd773807
Prazol Malla <prajwalmalla16@gmail.com>
2024-01-30 07:53:02 +0545

    send mail added

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const image = document.getElementsByClassName("images")
-    const overlay = document.querySelector(".onclick-overlay")
-    const closeBtn = document.getElementsByClassName("close-btn")
-    const doneBtn = document.getElementById("done-btn")
-    const successful = document.querySelector(".successful")
-    const numberForm = document.querySelector(".number-fill")
-    const datePicker = document.getElementById("date-picker")
-    const homeBtn = document.querySelector(".home")
-    const voilationBtn = document.querySelector(".voilation")
-    const main = document.querySelector(".main-container")
-    const voilationLog = document.querySelector(".voilation-container")
-
+    const image = document.getElementsByClassName("images");
+    const overlay = document.querySelector(".onclick-overlay");
+    const closeBtn = document.getElementsByClassName("close-btn");
+    const doneBtn = document.getElementById("done-btn");
+    const successful = document.querySelector(".successful");
+    const numberForm = document.querySelector(".number-fill");
+    const datePicker = document.getElementById("date-picker");
+    const homeBtn = document.querySelector(".home");
+    const voilationBtn = document.querySelector(".voilation");
+    const main = document.querySelector(".main-container");
+    const voilationLog = document.querySelector(".voilation-container");
+    const sendMail =document.querySelector(".send-mail-container");
+    const viewBtn = document.getElementsByClassName("view-btn");
     datePicker.valueAsDate = new Date();
     for (var i = 0; i < image.length; i++) {
         image[i].addEventListener("click", function() {
@@ -26,6 +27,13 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
     doneBtn.addEventListener("click",function(){
+        let number = " "
+        let zonalCode = document.getElementById("zonalCodeSelect").value;
+        let lotNumber = document.getElementById("lotNumberInput").value;
+        let vehicleType = document.getElementById("vehicleTypeSelect").value;
+        let vehicleId = document.getElementById("vehicleIdInput").value;
+        number = zonalCode+"-"+lotNumber+"-"+vehicleType+" "+vehicleId;    
+        console.log(number);
         successful.style.display ="flex";
         numberForm.style.display ="none";
     });
@@ -35,32 +43,41 @@ document.addEventListener("DOMContentLoaded", function() {
         voilationLog.style.display="none";
         voilationBtn.classList.remove("active")
         homeBtn.classList.add("active")
+        sendMail.style.display="none";
     });
     voilationBtn.addEventListener("click",function(){
         main.style.display="none";
+        sendMail.style.display="none";
         voilationLog.style.display="flex";
         voilationBtn.classList.add("active")
         homeBtn.classList.remove("active")
 
     });
+    for(var i= 0;i<viewBtn.length;i++){
+        viewBtn[i].addEventListener("click",function(){
+            voilationLog.style.display="none";
+            sendMail.style.display="flex";
+        });
+    }
+    
     
 });
 
 
 // function updateImages(images) {
-//     const imagesContainer = document.getElementById('number-img');
-//     // Create an unordered list for the image
+//     const imagesContainer = document.querySelector('.number-img');
 //     imagesContainer.innerHTML='';
 //     images.forEach(image => {
 //         const imgElement = document.createElement('img');
 //         imgElement.classList.add("images")
 //         const timestamp = new Date().getTime();  // Add a timestamp to force reload
 //         // Use the correct path to retrieve the image
-//         imgElement.src = /number_plate_images/${image}?t=${timestamp};
+//         imgElement.src = `/number_plate_images/${image}?t=${timestamp}`;
 //         imgElement.alt = 'Number Plate Image';
-//           // Attach click event to each image
 
 //         imagesContainer.appendChild(imgElement);
         
 //     });
 // }
+
+
